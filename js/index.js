ab785f52bb8244d18ff1257b7916924a869a794a
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-09 10:09:06 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 document.getElementById("TEXT").innerHTML = "HOI";
-/*const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 const gl = canvas.getContext("webgl2");
 if(!gl) {
   console.log("Failed to create a webGL2 context.");
 } else {
+  console.log("Webgl2 context succesfully generated.");
   gl.clearColor(0.2, 0.5, 0.3, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-}*/
+}
