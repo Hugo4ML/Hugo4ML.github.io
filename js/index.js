50d6407bc484c538ba3f523695203fdcd89b6115
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-09 11:39:10 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
 document.getElementById("TEXT").innerHTML = "HOI; BOYE";
 
 const canvas = document.getElementById("canvas");
-//canvas.width = document.body.clientWidth;
-//canvas.height = document.body.clientHeight;
 
 const gl = canvas.getContext("webgl2");
 if(!gl) {
   console.log("Failed to create a webGL2 context.");
 } else {
   console.log("Webgl2 context succesfully generated.");
-  gl.clearColor(0.2, 0.5, 0.3, 1.0);
+  gl.clearColor(0.5, 0.3, 0.2, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
