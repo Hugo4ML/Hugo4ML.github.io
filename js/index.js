2c81f7e5084e96e4eb48e2078e21a4364616692f
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-09 13:58:26 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const gl = canvas.getContext("webgl2");
 if(!gl) {
   console.log("Failed to create a webGL2 context.");
 } else {
-  console.log("Webgl2 context succesfully generated.");
+  let vertexShader = gl.createShader(gl.VERTEX_SHADER), fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.clearColor(0.5, 0.3, 0.2, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
