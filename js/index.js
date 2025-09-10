8896eed2a7bb33197f250bd7b0e3862a02997029
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-10 16:57:42 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.title = "(0.05) Simple project";
+document.title = "(0.06) Simple project";
 
 const canvas = document.getElementById("canvas");
 
@@ -9,7 +9,7 @@ if(!gl) {
   console.log("Failed to create a webGL2 context.");
 } else {
   fetch("../glsl/vertexShader.glvs").then((file => file.text())).then(console.log);
-  console.log(gl.createShader());
+  console.log(gl.createShader(gl.VERTEX_SHADER));
   gl.clearColor(0.5, 0.3, 0.2, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
