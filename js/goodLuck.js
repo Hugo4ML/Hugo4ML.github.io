013e1fe6f7f486a8bcd8a229ec1734f3e90bda84
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-10 22:10:34 +0200

    Update goodLuck.js

--- js/goodLuck.js
+++ js/goodLuck.js
@@ -1,9 +1,38 @@
 "use strict";
 
-function Shader(gl, path) {
+class Shader extends WebGLShader {
   /*
-  Creates an openGL shader from contents of GLSL file.
+  Manages webGL shader object.
   */
+  constructor(gl, path) {
+    /*
+    Generates and stores shader from data contained in file.
+    */
+    let shaderStage = undefined;
+    switch(path.slice(path.length - 5, path.length)) {
+    case ".glvs":
+      shaderStage = gl.VERTEX_SHADER;
+      break;
+    case ".glfs":
+      shaderStage = gl.FRAGMENT_SHADER;
+      break;
+    }
+    super(gl.createShader(shaderStage));
+    fetch(path).then(file => file.text()).then(text => gl.shaderSource(this, text));
+    gl.compileShader(this);
+    
+    if(!gl.getShaderParameter(gl.COMPILE_STATUS)) {
+      console.log(gl.glGetShaderInfoLog(this));
+      gl.deleteShader(this);
+    }
+  }
+}
+
+/*
+function Shader(gl, path) {
+  
+  Creates an openGL shader from contents of GLSL file.
+  
   let shaderStage = undefined;
   switch(path.slice(path.length - 5, path.length)) {
   case ".glvs":
@@ -22,7 +51,7 @@ function Shader(gl, path) {
     gl.deleteShader(this);
   }
 }
-
+*/
 function ShaderProgram(gl, vertexShader, fragmentShader) {
   /*
   Create a shader program attaching all specified shaders.
