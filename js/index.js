202aec7bc981b4e1007c86f29ace2bfe96574c62
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-10 17:28:31 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 import * as goodLuck from "./goodLuck.js";
 
-document.title = "(0.07) Simple project";
+document.title = "(0.08) Simple project";
 
 const canvas = document.getElementById("canvas");
 
@@ -13,6 +13,29 @@ if(!gl) {
   let vertexShader = new Shader(gl, "../glsl/vertexShader.glvs");
   let fragmentShader = new Shader(gl, "../glsl/fragmentShader.glfs");
   let shaderProgram = new ShaderProgram(gl, vertexShader, fragmentShader);
+
+  let positions = new Float32Array([
+    0, 0, 0,
+    0, 0.5, 0,
+    0.7, 0, 0
+  ]);
+  
+  let vbo = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+  
+  let positionL = gl.getAttribLocation("position");
+
+  let vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionL);
+  gl.vertexAttribPointer(positionL, 3, gl.FLOAT, false, 0, 0);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  
   gl.clearColor(0.5, 0.3, 0.4, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram();
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
