f7fc08b3df1a0a1e6700289e0a2fa575638af4b3
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-11 13:57:09 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -21,29 +21,36 @@ void main() {
 `;
 
 document.title = "(0.14) Simple project";
-
 const canvas = document.getElementById("canvas");
 
-const gl = canvas.getContext("webgl2");
-if(!gl) {
-  console.log("Failed to create a webGL2 context.");
-} else {
+async function main(canvas) {
+  /*
+  Main function. Declared as asynchronous to make better use of promises and read files.
+  */
+  const gl = canvas.getContext("webgl2");
+  if(!gl) {
+    console.log("Failed to create a webGL2 context.");
+    return;
+  }
+
   let vertexShader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShader, vertexShaderSource);
   //fetch("../glsl/vertexShader.glvs").then(file => file.text()).then(source => gl.shaderSource(vertexShader, source));
-  fetch("../glsl/vertexShader.glvs").then(file => file.text()).then(console.log);
+  //fetch("../glsl/vertexShader.glvs").then(file => file.text()).then(console.log);
   gl.compileShader(vertexShader);
   let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   //gl.shaderSource(fragmentShader, fragmentShaderSource);
-  gl.shaderSource(fragmentShader, fetch("../glsl/fragmentShader.glfs").then(file => file.text()).then(text => {lines = text.split("\n")}));
-  //fetch("../glsl/fragmentShader.glfs").then(file => file.text()).then(source => gl.shaderSource(fragmentShader, source));
-  fetch("../glsl/fragmentShader.glfs").then(file => file.text()).then(console.log);
-  gl.compileShader(fragmentShader);
+
+  let fragmentShaderFile = await fetch("../glsl/fragmentShader.glfs");
+  let fragmentShaderSource = await fragmentShaderFile.text();
+  gl.shaderSource(fragmentShader, await fragmentShaderSource);
+  
+  gl.compileShader(await fragmentShader);
   
   let program = gl.createProgram();
   gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
+  gl.attachShader(program, await fragmentShader);
+  gl.linkProgram(await program);
 
   let positions = new Float32Array([
     0, 0, 0,
@@ -55,7 +62,7 @@ if(!gl) {
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
   
-  let positionL = gl.getAttribLocation(program, "position");
+  let positionL = gl.getAttribLocation(await program, "position");
 
   let vao = gl.createVertexArray();
   gl.bindVertexArray(vao);
@@ -67,6 +74,9 @@ if(!gl) {
   gl.clearColor(0.5, 0.3, 0.4, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.useProgram(program);
+  gl.useProgram(await program);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
+
+main(canvas);
+
