e9da23c4d264bc2a7f709e32d279fe8afcf3b26c
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-18 17:53:47 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 import * as input from "./input.js";
 
-window.document.title = "(0.59) Simple project";
+window.document.title = "(0.60) Simple project";
 const canvas = window.document.getElementById("canvas");
 const gl = canvas.getContext("webgl2");
 
