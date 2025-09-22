91a1826fda7e08f48643d1a1f532954174770070
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-22 11:48:35 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.1.02) Simple project";
+  window.document.title = "(0.1.03) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
