c276dac2fc56cf947498b89f5fbbc8de780b7998
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-09-23 13:56:46 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -71,7 +71,8 @@ async function main() {
     0, 2, 3
   ]), gl.STATIC_DRAW);
   
-  gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 2 * 32 / 8, 0);
+  gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 5 * 32 / 8, 0);
+  gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 5 * 32 / 8, 2 * 32 / 8);
   
   let color = [0.5, 0.3, 0.4, 1.0];
   
